85a38fa1410cc3bbdae7cb460ce3f1b775f3bcc3
Lucas Ribeiro <82158676+Luqui05@users.noreply.github.com>
2024-07-17 20:38:23 -0300

    Update Main.java

--- DesafioSemProva/src/Desafio/Main.java
+++ DesafioSemProva/src/Desafio/Main.java
@@ -2,17 +2,29 @@ package Desafio;
 import java.util.List;
 import java.util.Scanner;
 
+import static Desafio.Menu.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite o caminho do arquivo: ");
         String caminho = sc.nextLine();
-        int opcao;
-        do {
-            Menu.imprimirMenu();
-            opcao = sc.nextInt();
-            List<Aluno> alunos = LeituraArquivo.tratamentoDados(caminho);
-            Menu.roteador(alunos, opcao);
-        } while (opcao != 0);
+        List<Aluno> alunos = LeituraArquivo.tratamentoDados(caminho);
+        System.out.println(calcularMediaDisciplinaPorAno(alunos, 2023));
+        System.out.println(calcularMedianaDisciplinaPorAno(alunos, 2023));
+        System.out.println(calcularDesvioPadraoDisciplinaPorAno(alunos, 2023));
+        System.out.println(calcularMediaDisciplinaPorAno(alunos, 2024));
+        System.out.println(calcularMedianaDisciplinaPorAno(alunos, 2024));
+        System.out.println(calcularDesvioPadraoDisciplinaPorAno(alunos, 2024));
+        System.out.println(calcularMediaDisciplina(alunos));
+        System.out.println(calcularMedianaDisciplina(alunos));
+        System.out.println(calcularDesvioPadraoDisciplina(alunos));
+        System.out.println(identificarMelhorDesempenho(alunos));
+        System.out.println(identificarPiorDesempenho(alunos));
+        System.out.println(determinarQuantidadeAlunos(alunos));
+        System.out.println(determinarAlunosAprovadosReprovados(alunos));
+        criarArquivoDeMedia(alunos);
+        criarArquivoDeAlunosAprovados(alunos);
+        criarArquivoDeAlunosReprovados(alunos);
     }
 }
